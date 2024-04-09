04470d0576d5d61ac783c456c31d08624dbed94b
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-04-09 11:36:41 -0400

    add homepage placeholder, css, connect google font

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,9 +1,14 @@
 function App() {
 
   return (
-    <div>
-      <h1>Ryan Hill Studio</h1>
-    </div>
+    <>
+      <header>
+        <h1 class="courier-prime-regular">Ryan Hill Studio</h1>
+      </header>
+      <footer>
+        <a href="https://www.instagram.com/ryanhillstudios/" target="_blank"><img src="https://www.ryanhill.studio/images/Instagram_Glyph_White.png" alt="Instagram logo" height="70px" /> </a>
+      </footer>
+    </>
   );
 }
 
