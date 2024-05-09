f915299057d20b88c0c4d288ec063d836a1d3852
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-05-09 15:23:32 -0400

    test CI/CD

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header>
         <h1 className="courier-prime-regular">Ryan Hill Studio</h1>
       </header>
-      <AudioTest></AudioTest>
+      <AudioTest>Hello</AudioTest>
       <footer>
         <a href="https://www.instagram.com/ryanhillstudios/" target="_blank"><img src="https://www.ryanhill.studio/images/Instagram_Glyph_White.png" alt="Instagram logo" className="size-20" /> </a>
       </footer>
