741e8c5c06fecddf6e862f97357a195034f2d82b
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-05-22 15:44:24 -0400

    fetch images from API using description to match series to parent page

--- src/client/components/Page.jsx
+++ src/client/components/Page.jsx
@@ -5,6 +5,7 @@ const Page = ({ parentPage, setParentPage }) => {
   const baseURL = import.meta.env.VITE_API;
   const [pageData, setPageData] = useState({});
   const [pageDescription, setPageDescription] = useState("Loading...");
+  const [pageImages, setPageImages] = useState([]);
   
 
   useEffect(() => {
@@ -23,7 +24,20 @@ const Page = ({ parentPage, setParentPage }) => {
       }
     };
 
+    const fetchImages = async () => {
+      try {
+        const { data } = await axios.get(`${baseURL}/media?search=series-${parentPage}`);
+        setPageImages(data);
+      }
+      catch (error) {
+        console.error("No images to be found.")
+      }
+    }
+
     fetchData(); 
+    fetchImages();
+    console.log(pageImages)
+
   }, [parentPage])
 
 
@@ -31,6 +45,23 @@ const Page = ({ parentPage, setParentPage }) => {
     <>
       <h5>This is the curent "parent" page: {parentPage.toUpperCase()}</h5>
       <div dangerouslySetInnerHTML={{ __html: pageDescription }} />
+      <div>
+      <h1>IMAGES</h1>
+      {pageImages.length > 0 ? (
+        // Render images if pageImages array is not empty
+        pageImages.map((pic) => (
+          <div key={pic.id} >
+            <img key={pic.id} src={pic.link} alt={pic.alt_text} style={{ maxHeight: "50vh" }}/>
+            <p>{pic.title.rendered}</p>
+            <div dangerouslySetInnerHTML={{ __html: pic.caption.rendered }} />
+          </div>
+        ))
+      ) : (
+        // Render loading message if pageImages array is empty
+        <h1></h1>
+        //Set something if no images are found. Perhaps time out images loading after 3 seconds and then show nothing.
+      )}
+    </div>
     </>
   )
 }
