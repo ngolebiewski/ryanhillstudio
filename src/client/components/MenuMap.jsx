fbee2a14a4a68fac77e532e20dc0fe8689221519
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-09-16 15:03:50 -0400

    stops all sounds

--- src/client/components/MenuMap.jsx
+++ src/client/components/MenuMap.jsx
@@ -23,10 +23,22 @@ const MenuMap = ({ isSoundOn, setIsSoundOn }) => {
     sounds[randomKey].play();  // Play the randomly selected sound
   };
 
+  const stopAllSounds = () => {
+    Object.values(sounds).forEach(sound => {
+      // Fade out from current volume to 0 over 0.25 seconds (250ms)
+      // sound.fade(sound.volume(), 0, 250);
+      // Stop the sound after the fade is complete
+      setTimeout(() => sound.stop(), 250);
+    });
+    
+  };
+
   const toggleSound = () => {
     setIsSoundOn(prevState => !prevState);  // Toggle the sound state
-    if (!isSoundOn) {  // If sound is currently off, play a random sound when toggled on
-      playRandomSound();
+    if (isSoundOn) {
+      stopAllSounds();  // Stop all sounds if toggling sound off
+    } else {
+      playRandomSound();  // Play a random sound if toggling sound on
     }
   };
 
@@ -59,47 +71,47 @@ const MenuMap = ({ isSoundOn, setIsSoundOn }) => {
       <div className='menu-art-container'>
         <svg width="100%" height="100%" viewBox="0 0 2024 766">
           <image href="/images/site/menu/Drawn-Menu-3.png" width="2024" height="766" />
-          <polygon 
-            points="635,148 670,24 1005,28 1001,140" 
-            fill="#FF000080" 
-            onMouseOver={() => handleAreaMouseOver('about')} 
-            onClick={() => handleAreaClick('about')} 
+          <polygon
+            points="635,148 670,24 1005,28 1001,140"
+            fill="#FF000080"
+            onMouseOver={() => handleAreaMouseOver('about')}
+            onClick={() => handleAreaClick('about')}
           />
-          <polygon 
-            points="658,234 660,155 712,149 1003,139 1039,124 1023,226 846,246" 
-            fill="#FFA50080" 
-            onMouseOver={() => handleAreaMouseOver('series')} 
-            onClick={() => handleAreaClick('series')} 
+          <polygon
+            points="658,234 660,155 712,149 1003,139 1039,124 1023,226 846,246"
+            fill="#FFA50080"
+            onMouseOver={() => handleAreaMouseOver('series')}
+            onClick={() => handleAreaClick('series')}
           />
-          <polygon 
-            points="789,332 783,243 992,225 1472,210 1461,316" 
-            fill="#FFFF0080" 
-            onMouseOver={() => handleAreaMouseOver('installation')} 
-            onClick={() => handleAreaClick('installation')} 
+          <polygon
+            points="789,332 783,243 992,225 1472,210 1461,316"
+            fill="#FFFF0080"
+            onMouseOver={() => handleAreaMouseOver('installation')}
+            onClick={() => handleAreaClick('installation')}
           />
-          <polygon 
-            points="890,427 907,327 1150,325 1282,323 1268,405 1112,423" 
-            fill="#00800080" 
-            onMouseOver={() => handleAreaMouseOver('studio')} 
-            onClick={() => handleAreaClick('studio')} 
+          <polygon
+            points="890,427 907,327 1150,325 1282,323 1268,405 1112,423"
+            fill="#00800080"
+            onMouseOver={() => handleAreaMouseOver('studio')}
+            onClick={() => handleAreaClick('studio')}
           />
-          <polygon 
-            points="905,509 917,429 1352,402 1337,481 1293,495" 
-            fill="#0000FF80" 
-            onMouseOver={() => handleAreaMouseOver('events')} 
-            onClick={() => handleAreaClick('events')} 
+          <polygon
+            points="905,509 917,429 1352,402 1337,481 1293,495"
+            fill="#0000FF80"
+            onMouseOver={() => handleAreaMouseOver('events')}
+            onClick={() => handleAreaClick('events')}
           />
-          <polygon 
-            points="881,584 876,513 1316,493 1370,481 1359,586" 
-            fill="#80008080" 
-            onMouseOver={() => handleAreaMouseOver('contact')} 
-            onClick={() => handleAreaClick('contact')} 
+          <polygon
+            points="881,584 876,513 1316,493 1370,481 1359,586"
+            fill="#80008080"
+            onMouseOver={() => handleAreaMouseOver('contact')}
+            onClick={() => handleAreaClick('contact')}
           />
-          <polygon 
-            points="828,668 826,590 1450,565 1446,692" 
-            fill="#00808080" 
-            onMouseOver={() => handleAreaMouseOver('commission')} 
-            onClick={() => handleAreaClick('commissions')} 
+          <polygon
+            points="828,668 826,590 1450,565 1446,692"
+            fill="#00808080"
+            onMouseOver={() => handleAreaMouseOver('commission')}
+            onClick={() => handleAreaClick('commissions')}
           />
         </svg>
       </div>
